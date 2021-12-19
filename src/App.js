74367fde1da22f81bf0f58a524b20c738db18761
v-wang <victorwang920@gmail.com>
2021-12-19 18:07:34 -0500

    map added with search

--- src/App.js
+++ src/App.js
@@ -6,22 +6,20 @@ import RequestForm from './components/RequestForm';
 import Status from './components/Status';
 import { useState, useEffect } from 'react';
 
-
 function App() {
-
-  const [searchString, setSearchString] = useState('')
+  const [searchString, setSearchString] = useState('');
 
   useEffect(() => {
     getAddress(searchString);
   }, []);
 
   function getAddress(searchString) {
-    // const url = //MAP API 
+    // const url = //MAP API
   }
 
-  fetch () 
-    .then(response => response.json())
-    .then(response => {
+  fetch()
+    .then((response) => response.json())
+    .then((response) => {
       setSearchString('');
     })
     .catch(console.error);
@@ -29,27 +27,24 @@ function App() {
   function handleChange(event) {
     setSearchString(event.target.value);
   }
-    
+
   function handleSubmit(event) {
     event.preventDefault();
     getAddress(searchString);
   }
-    
-  return (
-  <>
-    <nav className="App">
-  
-    </nav>
-    <main>
-      <Search 
-        handleChange={handleChange}
-        handleSubmit={handleSubmit}
-        searchString={searchString}/>
-      
-    </main>
-  </>
-  
 
+  return (
+    <>
+      <nav className='App'></nav>
+      <main>
+        <Search
+          handleChange={handleChange}
+          handleSubmit={handleSubmit}
+          searchString={searchString}
+        />
+        <MainMap />
+      </main>
+    </>
   );
 }
 
