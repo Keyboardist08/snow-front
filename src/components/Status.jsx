7cb27d6838d7e7309b2fa4b35012813101404dad
keyboardist08 <liyamelikyan@gmail.com>
2021-12-20 13:35:24 -0800

    added status-box and adjusted navigation

--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 
 function Status(props) {
     return (
-        <div>
-            Status Page
+        <div className="container">
+            <div className='list-box'>
+                <ul>
+                    <li>00-06 HOURS</li>
+                    <li>06-18 HOURS</li>
+                    <li>18-24//#endregion HOURS</li>
+                    <li>INCOMPLETE</li>
+                    <li>COMPLETE</li>
+                </ul>
+
+            </div>
         </div>
     );
 }
