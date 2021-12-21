554ffa5ab56dc48ca0b7c16b42e57b2e3d7f2228
keyboardist08 <liyamelikyan@gmail.com>
2021-12-21 14:12:21 -0800

    list label finished but still need to create checkboxes and color code new requests

--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import Collapse from 'react-bootstrap/Collapse'
-import {useState} from 'react';
-import Button from 'react-bootstrap/Button';
+// import Collapse from 'react-bootstrap/Collapse'
+// import Button from 'react-bootstrap/Button';
 import { Accordion, Container } from 'react-bootstrap';
 
 
@@ -10,33 +9,60 @@ function Status() {
   
     return (
       <Container fluid>
-      <Accordion  defaultActiveKey="0">
-  <Accordion.Item eventKey="0">
-    <Accordion.Header>Accordion Item #1</Accordion.Header>
-    <Accordion.Body>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
-    </Accordion.Body>
-  </Accordion.Item>
-  <Accordion.Item eventKey="1">
-    <Accordion.Header>Accordion Item #2</Accordion.Header>
-    <Accordion.Body>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
-    </Accordion.Body>
-  </Accordion.Item>
-</Accordion>
-</Container>
+        <Accordion  defaultActiveKey="0">
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>NEW REQUESTS</Accordion.Header>
+            <Accordion.Body>
+              {/* Still need to create checkboxes and color-codes next to each address */}
+              <ul>
+                <li>Address</li>
+                <li>Address</li>
+                <li>Address</li>
+              </ul>
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="1">
+          <Accordion.Header>06-18 HOURS</Accordion.Header>
+            <Accordion.Body>
+              <ul>
+                <li>Address</li>
+                <li>Address</li>
+                <li>Address</li>
+              </ul>
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="2">
+          <Accordion.Header>18-24 HOURS</Accordion.Header>
+            <Accordion.Body>
+              <ul>
+                <li>Address</li>
+                <li>Address</li>
+                <li>Address</li>
+              </ul>
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="3">
+          <Accordion.Header>INCOMPLETE</Accordion.Header>
+            <Accordion.Body>
+              <ul>
+                <li>Address</li>
+                <li>Address</li>
+                <li>Address</li>
+              </ul>
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="4">
+          <Accordion.Header>COMPLETE</Accordion.Header>
+            <Accordion.Body>
+              <ul>
+                <li>Address</li>
+                <li>Address</li>
+                <li>Address</li>
+              </ul>
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      </Container>
     //   <div className ="container">
     //        <Button className="btn btn-primary">INCOMING REQUESTS</Button>
     //      <div className="content">
