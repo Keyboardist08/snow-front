bbc2b15cf8072005daa22e933367a004ddfbc45c
keyboardist08 <liyamelikyan@gmail.com>
2021-12-21 10:48:42 -0800

    Finally fixed positions of footer and search components on top

--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
+import Collapse from 'react-bootstrap/Collapse'
+import {useState} from 'react';
+import { Button } from 'bootstrap';
 
-function Status(props) {
+
+
+function Status() {
+  
     return (
-        <div className="container">
-            <div className='list-box'>
-                <ul>
-                    <li>00-06 HOURS</li>
-                    <li>06-18 HOURS</li>
-                    <li>18-24//#endregion HOURS</li>
-                    <li>INCOMPLETE</li>
-                    <li>COMPLETE</li>
-                </ul>
-
-            </div>
-        </div>
+      <div className ="container">
+           <button class="collapsible">INCOMING REQUESTS</button>
+         <div class="content">
+           <li>1234 Graham Rd. Brooke, PA 19011</li>
+          </div>
+         <button class="collapsible">06-18 HOURS</button>
+          <div class="content">
+           <li>1234 Graham Rd. Brooke, PA 19011</li>
+         </div>
+        <button class="collapsible">18-24 HOURS</button>
+          <div class="content">
+            <li>1234 Graham Rd. Brooke, PA 19011</li>
+         </div>
+        <button class="collapsible">INCOMPLETE</button>
+          <div class="content">
+          <li>1234 Graham Rd. Brooke, PA 19011</li>
+          </div>
+           <button variant="primary" class="collapsible">COMPLETE</button>
+          <div class="content">
+           <li>1234 Graham Rd. Brooke, PA 19011</li>
+          </div>
+    </div> 
     );
 }
 
