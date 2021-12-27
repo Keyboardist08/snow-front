ce2dde6ad36866374391704b87d20e539a1ab3eb
jaygilot9@gmail.com <jaygilot9@gmail.com>
2021-12-27 11:11:55 -0500

    rebase

--- src/components/MainMap.jsx
+++ src/components/MainMap.jsx
@@ -1,38 +1,38 @@
-import React from 'react';
-import Leaflet from 'leaflet';
-import 'leaflet/dist/leaflet.css';
-import { Button } from 'react-bootstrap';
+import React from "react";
+import Leaflet from "leaflet";
+import "leaflet/dist/leaflet.css";
+import { Button } from "react-bootstrap";
 import {
   MapContainer,
   TileLayer,
   Marker,
   Popup,
   MapConsumer,
-} from 'react-leaflet';
+} from "react-leaflet";
 // import SeedData from './map-seed.json';
-import { useState, useEffect } from 'react';
-import { FaSnowflake } from 'react-icons/fa';
+import { useState, useEffect } from "react";
+import { FaSnowflake } from "react-icons/fa";
 
 // redirect icons
-Leaflet.Icon.Default.imagePath = '../node_modules/leaflet';
+Leaflet.Icon.Default.imagePath = "../node_modules/leaflet";
 delete Leaflet.Icon.Default.prototype._getIconUrl;
 Leaflet.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
 });
 
 function MainMap() {
   const [geoAddress, setGeoAddress] = useState({});
   const [center, setCenter] = useState([39.9526, -75.1652]);
   const [testData, setTestData] = useState([]);
-  const [inputAddress, setInputAddress] = useState('');
+  const [inputAddress, setInputAddress] = useState("");
   const inputAddressHandler = (ev) => {
-    setInputAddress(ev.target.value.replaceAll(' ', '+'));
+    setInputAddress(ev.target.value.replaceAll(" ", "+"));
   };
 
   useEffect(() => {
-    fetch('https://snowfall-back-end.herokuapp.com/')
+    fetch("https://snowfall-back-end.herokuapp.com/")
       .then((response) => {
         return response.json();
       })
@@ -57,10 +57,10 @@ function MainMap() {
       })
       .then((addressMatch) => {
         console.log(addressMatch);
-        fetch('https://snowfall-back-end.herokuapp.com/', {
-          method: 'POST',
+        fetch("http://localhost:3000/", {
+          method: "POST",
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
           body: JSON.stringify(addressMatch),
         });
@@ -75,29 +75,29 @@ function MainMap() {
 
   return (
     <div>
-      <h1 className='title'>
+      <h1 className="title">
         Snow Helper Proto <FaSnowflake></FaSnowflake>
       </h1>
-      <form className='App'>
+      <form className="App">
         <label></label>
         <input
-          className='input'
-          name='input-address'
-          id='input-address'
+          className="input"
+          name="input-address"
+          id="input-address"
           onChange={(ev) => inputAddressHandler(ev)}
         ></input>
         <Button
-          id='button'
-          className='btn btn-primary'
-          value='Submit'
-          type='submit'
+          id="button"
+          className="btn btn-primary"
+          value="Submit"
+          type="submit"
           onClick={(ev) => {
             ev.preventDefault();
             getGeoAddress();
-            setInputAddress('');
+            setInputAddress("");
           }}
         >
-          <i class='bi bi-geo-alt-fill'></i>
+          <i class="bi bi-geo-alt-fill"></i>
           Request
         </Button>
       </form>
@@ -109,12 +109,12 @@ function MainMap() {
         zoom={13}
         scrollWheelZoom={false}
         // needs height to render
-        style={{ height: '50vh', width: '100vw' }}
+        style={{ height: "50vh", width: "100vw" }}
       >
         <TileLayer
           // https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-          url='https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png'
+          url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
           maxZoom={20}
         />
         <MapConsumer>
